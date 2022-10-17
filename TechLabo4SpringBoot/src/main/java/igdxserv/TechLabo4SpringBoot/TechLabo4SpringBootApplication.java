a5062f970753a80578a896221a1a8c82b97ebcb1
udonkot <udonko104@gmail.com>
2022-10-17 16:35:19 +0900

    first commit

---
+++ TechLabo4SpringBoot/src/main/java/igdxserv/TechLabo4SpringBoot/TechLabo4SpringBootApplication.java
@@ -0,0 +1,13 @@
+package igdxserv.TechLabo4SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechLabo4SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechLabo4SpringBootApplication.class, args);
+	}
+
+}
---
+++ TechLabo4SpringBoot/src/test/java/igdxserv/TechLabo4SpringBoot/TechLabo4SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package igdxserv.TechLabo4SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechLabo4SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
